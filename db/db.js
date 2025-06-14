87650fd07e35619e58bc8afc123b5b24366da110
Meike Ahlers <meike.ahlers23@gmail.com>
2025-06-14 14:07:10 +1200

    update pool config

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "me",
-  host: "localhost",
-  database: "ecommerce_shop",
-  password: "password",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   port: 5432,
 });
 
